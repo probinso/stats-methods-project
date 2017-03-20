9946c904a2c93adfb58eb0fb4f14e6f0a3d94a71
probinso <pmoss.robinson@gmail.com>
2017-03-20 12:03:06 -0700

    forgot to add

---
+++ setup.R
@@ -0,0 +1,88 @@
+#!/usr/bin/env R
+
+library(caret)
+library(matrixStats)
+library(reshape2)
+library(tidyverse)
+
+################################################################
+
+part   = function(f, ...) function(X) f(X, ...) # parameter-partial evaluation
+
+'%ni%' = Negate("%in%")
+'%&%'  = function(x, y)paste0(x,y) # string concatination
+
+'%|%'  = function(f, g) function(X) X %>% f %>% g # function composition
+'%T%'  = function(d, funcs) lapply(funcs, part(function(f, d) f(d), d)) # Tee
+'%T>%' = function(l, f) lapply(l, f) # lapply after Tee (consistant syntax)
+
+draw_rownames <- function(.data) .data %>%
+  do(mutate(.,"rownames"=rownames(.)))
+
+read.tsv = part(read.delim, sep='\t', row.names=1, header=T, check.names = F)
+
+pop_header_row = function (df) {
+  # malformed data, encoded labels in top row
+  df.new = df
+  colnames(df.new) = df.new[1,]
+  df.new[-1,]
+}
+
+dropcols = function(df, cols) df[,colnames(df) %ni% cols]
+
+matrix_boxplot = as.data.frame %|% stack %|%
+  function(x) ggplot(x) + geom_boxplot() +
+  aes(x=ind, y=values) + ylim(c(2,14)) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+
+################################################################
+
+gene_data = read.tsv("./../data/expression.txt") %>% 
+  as.matrix %>% t
+
+training = read.tsv("./../data/training_set_answers.txt") %>% 
+  `==`(1) %>% ifelse(T, F)
+
+subtypes = read.tsv("./../data/subtypes.txt")
+# read and re-structure training_set_answers from disk
+targets  = read.tsv("./../data/training_set_answers.txt") 
+
+mapping = read.csv("./../data/scoring_and_test_set_id_mappings.csv") %>%
+  data.frame
+
+
+drugs = colnames(training)
+names(drugs) = drugs
+
+rowCov = function(mat) rowSds(mat) / rowMeans(mat)
+colCoV = t %|% rowCov
+
+gene_cov = gene_data %>% colCoV
+
+
+################################################################
+CVTR = 0.4
+genes_cov_thresh =
+  function(CVTR=0.4) gene_cov %>% subset(gene_cov > CVTR) %>% names
+
+gene_cov %>% qplot(.) + geom_vline(xintercept = CVTR, col="red")
+
+target_genes  = gene_cov %>% subset(gene_cov > CVTR) %>% names
+train_samples = row.names(training)
+test_samples  = rownames(gene_data)[rownames(gene_data) %ni% train_samples]
+
+data_by_genes  = function(genes) gene_data[, genes] %>% data.frame
+train_by_genes = function(genes) gene_data[train_samples, genes] %>% data.frame
+test_by_genes  = function(genes)
+  gene_data[test_samples, genes] %>% data.frame
+  
+#target_genes %>% train_by_genes
+#target_genes %>% test_by_genes
+
+success_by_drug = function(drug) training[,drug] %>% `==`(T) %>% ifelse('Y', 'N')
+
+train_by_drug = function(genes, drug)
+  genes %>% train_by_genes %>%
+  cbind(success=success_by_drug(drug))
+
