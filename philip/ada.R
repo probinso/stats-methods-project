9dab92e979526cdde06a063783e2c147ffd3530f
Lawrence Hsu <lawrenceh3@gmail.com>
2017-03-22 20:00:13 -0700

    adaboost on church

---
+++ philip/ada.R
@@ -0,0 +1,56 @@
+setwd("~/git/kaggle-stats")
+source(file.path("setup.R"))
+library(caret)
+library(memoise)
+library(doMC)
+
+library(easyGgplot2)
+library(ggplot2)
+source(file.path("multiplot.R"))
+
+
+registerDoMC(18)
+
+makedf = memoise(function(drug) {
+  genes_cov_thresh(0.2) %>%
+    train_by_drug(drug) %>% hotextend_subtypes %>%
+    mutate_all(as.numeric) %>% sort_cor_target(0.3, "success") %>%
+    mutate(success=as.factor(ifelse(success==1, 'Y', 'N')))
+})
+
+fmodels = lapply(
+  drugs,
+  function(drug) {
+    print("start:feature:" %&% drug)
+    df = makedf(drug)
+
+    rfctrl  = rfeControl(
+      functions=caretFuncs,
+      method="repeatedcv", number=3, repeats=5)
+
+    fmodel = rfe(
+      success ~ ., data = df,
+      rfeControl=rfctrl, method="rf", sizes=seq(3, 25, 4))
+    print("stop:feature:" %&% drug)
+    fmodel
+  }
+)
+
+
+Adamodels =
+  lapply(names(fmodels), function(drug) {
+    print("start:training:" %&% drug)
+
+    df = makedf(drug)
+    features = predictors(fmodels[[drug]])
+    
+    control = trainControl(method="repeatedcv", number=3, repeats=5)
+
+    rf_default = train(
+      success ~ ., data=df,
+      method="ada", trControl=control)
+    print("stop:training:" %&% drug)
+    rf_default
+  })
+names(Adamodels) = drugs
+
